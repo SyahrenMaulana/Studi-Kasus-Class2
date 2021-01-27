27a130e6d256f4021d457738b05ecaafe5e14223
SyahrenMaulana <sfa.asus@gmail.com>
2021-01-27 08:50:47 +0700

    first commit

---
+++ src/studikasusmethod2/Iya.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studikasusmethod2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Iya {
+    //variabel
+    String nama = "Syahren Maulana Adil Hakim";
+    int NIK = 1234567890;
+    String alamat = "Kediri";
+    String ttl = "Kediri 4 Februari 2005";
+    
+    String nama(){
+        return nama;
+    }
+    
+    
+    int NIK(){
+        return NIK;
+    }
+    
+    String alamat(){
+        return alamat;
+    }
+    
+    String ttl(){
+        return ttl;
+    }
+    
+}
---
+++ src/studikasusmethod2/StudiKasusMethod2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studikasusmethod2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class StudiKasusMethod2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //pemanggilan
+        Iya oke = new Iya();
+        System.out.println("Nama :"+oke.nama());
+        System.out.println("NIK :"+oke.NIK());
+        System.out.println("Alamat :"+oke.alamat());
+        System.out.println("TTL :"+oke.ttl());
+    }
+    
+}
